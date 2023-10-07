344cb02d31024c667962def4e9b7b8fcaa495c08
LakhanRaut <lakhanraut24@gmail.com>
2023-10-07 17:13:43 +0530

    we learn html dom manipulation by using javascript and react

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
+// Create nested React Elements
+// Manipulate the HTML DOM using React
 const heading = React.createElement(
     "h1",
-    {},
-    "namaste react"
-);
-const root = ReactDom.createRoot(document.getElementById('root'));
-root.render(heading);
+    {id: "title",},
+    "hello ✋hii"
+  );
+  const heading2 = React.createElement(
+    "h2",
+    {id: "title2",className:"container",style: {
+        "color" : "blue",
+        "backgroundColor" : "yellow"
+    }},
+    "welcome to react inception"
+  );
+  const heading3 = React.createElement(
+    "h3",
+    {id: "title",},
+    "lets start code byh hand on practice"
+  );
+  const container = React.createElement(
+    "div",
+    {id: "container"
+,},
+    [heading,heading2,heading3]
+  );
+ 
+ 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
+// in previous version 17
+//ReactDOM.render(heading , document.getElementById("root")); 
+// -----------------------------------------------------
+// Manipulate the HTML DOM using Javscript
 
-console.log(heading);
+// const heading = document.createElement('h2');
+// heading.innerHTML = "namaste react";
+// const root = document.getElementById("root");
+// root.appendChild(heading);
